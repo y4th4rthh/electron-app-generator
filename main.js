e8535b12a3e4d35afc9505ea08756ba90e79472e
Patel Yatharth <114477915+y4th4rthh@users.noreply.github.com>
2025-10-16 14:11:53 +0530

    Modify Electron window settings and cleanup
    Updated window settings and removed autoUpdater.

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
-const { autoUpdater } = require('electron-updater');
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 1280,
-    height: 800,     
-    icon: path.join(__dirname, 'neura.ai.ico'),
+    height: 800,
+    icon: path.join(__dirname, 'neura.ico'),
+     autoHideMenuBar: true,
     webPreferences: {
       contextIsolation: true,
     },
   });
 
-  win.setMenuBarVisibility(false);
-
   // Load your built React app
   win.loadFile(path.join(__dirname, 'build', 'index.html'));
 
-  autoUpdater.checkForUpdatesAndNotify();
-
+  // win.webContents.openDevTools();
 }
 
 app.whenReady().then(createWindow);
+
+ // Open Chrome DevTools
+
